fbb72362c339222b4fc0cdcf1af8cd352804caa5
highballplz <kmkim405@gmail.com>
2024-04-24 16:32:34 +0900

    [Feat] mergeTailwindClassNames 유틸함수 더 간단하게 완성

--- src/utils/mergeTailwindClassNames.js
+++ src/utils/mergeTailwindClassNames.js
@@ -1,5 +1,59 @@
-const propertiesToBeMerged = ['textColor', 'bgColor'];
+export default function mergeTailwindClassNames(...classNames) {
+  //변수 이름이 's'로 끝나면 array, 아니면 string
 
+  // classNames = [
+  //   'w-1 h-2 text-black flex',
+  //   'w-3 h-4 text-white flex-row',
+  //   'w-5 h-6 text-transparent justify-between',
+  // ];
+
+  const joinedClassName = classNames.join(' ');
+  // joinedClassName =
+  //   'w-1 h-2 text-black flex w-3 h-4 text-white flex-row w-5 h-6 text-transparent justify-between';
+
+  const joinedClassNames = joinedClassName.split(' ');
+  // joinedClassNames = [
+  //   'w-1',
+  //   'h-2',
+  //   'text-black',
+  //   'flex',
+  //   'w-3',
+  //   'h-4',
+  //   'text-white',
+  //   'flex-row',
+  //   'w-5',
+  //   'h-6',
+  //   'text-transparent',
+  //   'justify-between',
+  // ];
+
+  const mergedClassNames = joinedClassNames.filter((className, index) => {
+    if (isTextColor(className))
+      return index === findTextColorIndex(joinedClassNames);
+    if (isBgColor(className))
+      return index === findBgColorIndex(joinedClassNames);
+    if (isWidth(className)) return index === findWidthIndex(joinedClassNames);
+    if (isHeight(className)) return index === findHeightIndex(joinedClassNames);
+    return true;
+  });
+  // mergedClassNames = [
+  //   'w-1',
+  //   'h-2',
+  //   'text-black',
+  //   'flex',
+  //   'flex-row',
+  //   'justify-between',
+  // ];
+
+  const mergedClassName = mergedClassNames.join(' ');
+  // mergedClassName = 'w-1 h-2 text-black flex flex-row justify-between';
+
+  return mergedClassName;
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                   colors                                   */
+/* -------------------------------------------------------------------------- */
 const defaultColors = [
   'inherit',
   'current',
@@ -48,81 +102,50 @@ const customColors = Object.keys({
 
 const colors = [...defaultColors, ...customColors];
 
-export default function mergeTailwindClassNames(
-  defaultClassName,
-  customClassName
-) {
-  //string으로 된 통짜 className을 array인 classNames로 변환
-  const defaultClassNames = defaultClassName.split(' ');
-  const customClassNames = customClassName.split(' ');
-
-  //classNames 배열을 classNameMap 객체로 변환 (단 예를 들어 text-red와 bg-red는 textColor: red와 bgColor: red로.)
-  const defaultClassNameMap = classNamesToClassNameMap(defaultClassNames);
-  const customClassNameMap = classNamesToClassNameMap(customClassNames);
-
-  //예를 들어, 만약 defaultClassNameMap과 customClassNameMap 모두에 textColor 프로퍼티가 있다면 defaultClassNameMap에서는 textColor 프로퍼티 삭제
-  propertiesToBeMerged.forEach((property) => {
-    if (isSharedProperty(defaultClassNameMap, customClassNameMap, property))
-      delete defaultClassNameMap[property];
-  });
-
-  //defaultClassNameMap 객체를 defaultClassNames 배열로 변환 (단 propertiesToBeMerged 이름은 원래 tailwind property 이름으로.)
-  const thinnerDefaultClassNames =
-    ClassNameMapToClassNames(defaultClassNameMap);
-  const mergedClassNames = `${thinnerDefaultClassNames.join(' ')} ${customClassNames.join(' ')}`;
-
-  return mergedClassNames;
+/* -------------------------------------------------------------------------- */
+/*                                  textcolor                                 */
+/* -------------------------------------------------------------------------- */
+function findTextColorIndex(classNames) {
+  return classNames.findIndex(isTextColor);
 }
 
-function classNamesToClassNameMap(classNames) {
-  const classNameMap = {};
-  classNames.forEach((className) => {
-    //예를 들어 'bg-red-100'을 ['bg','red','100']으로 만들어서 각 엔트리를 차례대로 property, value1, value2로 놓기.
-    let [property, value1, value2] = className.split('-');
-
-    //예를 들어 'bg-red-100'의 'bg'를 'bgColor'로 수정
-    if (colors.includes(value1)) {
-      switch (property) {
-        case 'text':
-          property = 'textColor';
-          break;
-        case 'bg':
-          property = 'bgColor';
-          break;
-      }
-    }
-
-    classNameMap[property] = !value2
-      ? `${value1}` //만약 value1도 없다면 그냥 ''이 들어감
-      : `${value1}-${value2}`;
-  });
-
-  return classNameMap;
+function isTextColor(className) {
+  const [property, value1, value2] = className.split('-');
+  return property === 'text' && colors.includes(value1); //text로 시작하는 다른 프로퍼티(textAlign, textWrap)가 있기 때문에 value가 color인지도 체크를 해줘야 됨.
 }
 
-function isSharedProperty(obj1, obj2, property) {
-  return hasProperty(obj1, property) && hasProperty(obj2, property);
+/* -------------------------------------------------------------------------- */
+/*                                   bgColor                                  */
+/* -------------------------------------------------------------------------- */
+function findBgColorIndex(classNames) {
+  return classNames.findIndex(isBgColor);
 }
 
-function hasProperty(obj, property) {
-  return Object.keys(obj).includes(property);
+function isBgColor(className) {
+  const [property, value] = className.split('-');
+  return property === 'bg' && colors.includes(value);
 }
 
-function ClassNameMapToClassNames(ClassNameMap) {
-  const classNames = [];
+/* -------------------------------------------------------------------------- */
+/*                                    width                                   */
+/* -------------------------------------------------------------------------- */
+function findWidthIndex(classNames) {
+  return classNames.findIndex(isWidth);
+}
 
-  Object.entries(ClassNameMap).forEach((classEntry, index) => {
-    let [property, value] = classEntry;
-    switch (property) {
-      case 'textColor':
-        property = 'text';
-        break;
-      case 'bgColor':
-        property = 'bg';
-        break;
-    }
-    classNames[index] = `${property}-${value}`;
-  });
+function isWidth(className) {
+  const [property] = className.split('-');
+  return property === 'w';
+}
 
-  return classNames;
+/* -------------------------------------------------------------------------- */
+/*                                    height                                   */
+/* -------------------------------------------------------------------------- */
+function findHeightIndex(classNames) {
+  return classNames.findIndex(isHeight);
+}
+
+function isHeight(className) {
+  const [property] = className.split('-');
+  return property === 'h';
 }
